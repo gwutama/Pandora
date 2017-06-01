475384e14b1a62df3ec47f5e067370cb07121aef
Galuh Utama <galuh.utama@gwutama.de>
2017-06-01 21:15:20 +0200

    Implemented indent/outdent in markdown editor with tab and backtab keys.

--- src/markdowneditor/markdowntextedit.cpp
+++ src/markdowneditor/markdowntextedit.cpp
@@ -1,6 +1,115 @@
 #include "markdowntextedit.h"
+#include <QDebug>
 
-MarkdownTextEdit::MarkdownTextEdit()
+const char* MarkdownTextEdit::sTag = "[MarkdownTextEdit]";
+
+MarkdownTextEdit::MarkdownTextEdit(QWidget* parent) :
+    QPlainTextEdit(parent)
 {
+}
 
+
+MarkdownTextEdit::~MarkdownTextEdit()
+{
+}
+
+
+void MarkdownTextEdit::keyPressEvent(QKeyEvent* event)
+{
+    if (event->key() == Qt::Key_Tab)
+    {
+        indent(event);
+        return;
+    }
+    else if (event->key() == Qt::Key_Backtab)
+    {
+        outdent(event);
+        return;
+    }
+
+    QPlainTextEdit::keyPressEvent(event);
+}
+
+
+void MarkdownTextEdit::indent(QKeyEvent* event)
+{
+    QTextCursor currentCursor = textCursor();
+    QString currentText = currentCursor.selectedText();
+
+    if (currentText.isEmpty())
+    {
+        currentCursor.insertText("  " + currentText);
+        event->accept();
+        return;
+    }
+    else
+    {
+        QStringList lines = currentText.split(QChar::ParagraphSeparator);
+        QString newText;
+
+        foreach (QString line, lines)
+        {
+            newText += "  " + line + "\n";
+        }
+
+        newText.chop(1);
+        currentCursor.insertText(newText);
+        event->accept();
+        return;
+    }
+}
+
+
+void MarkdownTextEdit::outdent(QKeyEvent *event)
+{
+    QTextCursor currentCursor = textCursor();
+    QString currentText = currentCursor.selectedText();
+
+    if (currentText.isEmpty())
+    {
+        for (int i = 0; i < 2; i++)
+        {
+            currentCursor.movePosition(QTextCursor::Left);
+            QChar c = document()->characterAt(currentCursor.position());
+
+            if (c == ' ')
+            {
+                currentCursor.deleteChar();
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        event->accept();
+        return;
+    }
+    else
+    {
+        QStringList lines = currentText.split(QChar::ParagraphSeparator);
+        QString newText;
+
+        foreach (QString line, lines)
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                if (line.startsWith(' '))
+                {
+                    line.remove(0, 1);
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            newText += line + "\n";
+        }
+
+        newText.chop(1);
+        currentCursor.insertText(newText);
+        event->accept();
+        return;
+    }
 }
--- src/markdowneditor/markdowntextedit.h
+++ src/markdowneditor/markdowntextedit.h
@@ -1,12 +1,25 @@
 #ifndef MARKDOWNTEXTEDIT_H
 #define MARKDOWNTEXTEDIT_H
 
-#include <QObject>
+#include <QPlainTextEdit>
 
 class MarkdownTextEdit : public QPlainTextEdit
 {
+    Q_OBJECT
+
 public:
-    MarkdownTextEdit();
+    explicit MarkdownTextEdit(QWidget* parent = 0);
+    virtual ~MarkdownTextEdit();
+
+protected slots:
+    void keyPressEvent(QKeyEvent* event);
+
+private:
+    void indent(QKeyEvent* event);
+    void outdent(QKeyEvent* event);
+
+private:
+    static const char* sTag;
 };
 
 #endif // MARKDOWNTEXTEDIT_H
