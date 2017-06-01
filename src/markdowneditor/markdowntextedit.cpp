a66e093a450daf960647f7dcff8fafb23fa5eced
Galuh Utama <galuh.utama@gwutama.de>
2017-06-01 13:40:15 +0200

    Implemented indenting and outdenting line(s) with tab and backtab.

---
+++ src/markdowneditor/markdowntextedit.cpp
@@ -0,0 +1,6 @@
+#include "markdowntextedit.h"
+
+MarkdownTextEdit::MarkdownTextEdit()
+{
+
+}
---
+++ src/markdowneditor/markdowntextedit.h
@@ -0,0 +1,12 @@
+#ifndef MARKDOWNTEXTEDIT_H
+#define MARKDOWNTEXTEDIT_H
+
+#include <QObject>
+
+class MarkdownTextEdit : public QPlainTextEdit
+{
+public:
+    MarkdownTextEdit();
+};
+
+#endif // MARKDOWNTEXTEDIT_H
