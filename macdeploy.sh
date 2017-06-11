33eb858211158ff920d924d21a0e3958d3092da4
Galuh Utama <galuh.utama@gwutama.de>
2017-06-11 14:59:45 +0200

    macdeployqt has bugs that causes qrc cannot be loaded anymore. In macdeploy.sh, therefore macdeployqt execution is commented out.

--- macdeploy.sh
+++ macdeploy.sh
@@ -1,14 +1,21 @@
 #!/usr/bin/env bash
 
 # CHANGEME
-export CMAKE_PREFIX_PATH=/Users/galuh/Qt/5.7/clang_64
-MACDEPLOYQT_BIN=/Users/galuh/Qt/5.7/clang_64/bin/macdeployqt
+CMAKE_PREFIX_PATH=/Users/galuh/Qt/5.9/clang_64
 
+
+QMAKE_BIN=$CMAKE_PREFIX_PATH/bin/qmake
 
 rm -rf build
 mkdir build
 cd build
-cmake ..
+QT_QMAKE_EXECUTABLE=$QMAKE_BIN CMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH cmake -DCMAKE_BUILD_TYPE=Release ..
 make -j5
+
 cp -r ../3rdparty/MacOS/ src/Pandora.app/Contents/MacOS/
-$MACDEPLOYQT_BIN src/Pandora.app -dmg
+
+# macdeployqt must be run from qt bin directory so that qrc still works!
+# see https://forum.qt.io/topic/59030/solved-macdeployqt-problem/9
+#BUILD_DIR=$(pwd)
+#cd $CMAKE_PREFIX_PATH/bin/
+#./macdeployqt $BUILD_DIR/src/Pandora.app
