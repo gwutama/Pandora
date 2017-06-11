8cd454b475a87faaae9813eda98cb828542fc6c5
Galuh Utama <galuh.utama@gwutama.de>
2017-06-11 14:16:14 +0200

    Small cleanups

--- macdeploy.sh
+++ macdeploy.sh
@@ -8,7 +8,7 @@ MACDEPLOYQT_BIN=/Users/galuh/Qt/5.7/clang_64/bin/macdeployqt
 rm -rf build
 mkdir build
 cd build
-cmake -DCMAKE_BUILD_TYPE=Release ..
+cmake ..
 make -j5
 cp -r ../3rdparty/MacOS/ src/Pandora.app/Contents/MacOS/
 $MACDEPLOYQT_BIN src/Pandora.app -dmg
